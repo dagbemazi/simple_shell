ac7afd107bd119d1ed6643ae27b9bc33e0d84ee4
[YOUR_GITHUB_USERNAME] <[YOUR_EMAIL_ON_GITHUB]>
2023-11-09 10:36:31 +0000

    chore: boiler plate

---
+++ main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * main -
+  *
+  * @argc:
+  * @argv:
+  * @envp:
+  *
+  * Description:
+  *
+  *
+  *
+  *
+  * Return:
+  */
+
+int main(int argc, char *argv[], char *envp[])
+{
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* library functions used */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+
+#endif
